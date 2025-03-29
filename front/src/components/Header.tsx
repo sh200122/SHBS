60aa4efcfcfa76cfa65d349c03424cabaf890a60
Alan <Alan.Shi@duomai.com>
2025-03-29 11:00:47 +0800

    解决 Header.tsx 的合并冲突

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
-/*
- * @Author: alan alan.shi@duomai.com
- * @Date: 2025-03-05 14:52:51
- * @LastEditors: alan alan.shi@duomai.com
- * @LastEditTime: 2025-03-05 15:34:21
- * @FilePath: \front\src\components\Header.tsx
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-import { View, Text, Image, Input } from "@tarojs/components";
-=======
 import { View, Text, Image, Input } from "@tarojs/components";
 import Taro from "@tarojs/taro";
->>>>>>> origin/sh
 import React from "react";
 
 type Props = {};
@@ -29,15 +17,6 @@ export default function Header({}: Props) {
         src="https://github.com/sh200122/SHBS/blob/main/front/public/images/logo.png?raw=true"
         className="w-20 h-20"
       />
-<<<<<<< HEAD
-      {/* 搜索框 */}
-      <View className="flex items-center justify-center w-[55%] mr-28">
-        <Input
-          type="text"
-          placeholder="🔍"
-          className="w-full border-solid border-[1px] p-2 border-gray-300 rounded"
-        />
-=======
       <View className="flex items-center justify-center w-[55%] mr-28">
         <View
           className="w-full border-solid border-[1px] p-2 border-gray-300 rounded bg-gray-50 text-gray-400"
@@ -45,7 +24,6 @@ export default function Header({}: Props) {
         >
           🔍 搜索商品
         </View>
->>>>>>> origin/sh
       </View>
     </View>
   );
