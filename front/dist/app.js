40a34480b54ad287bc8b7a3be0b03be821a01a61
Alan <Alan.Shi@duomai.com>
2025-03-29 11:49:44 +0800

    新分支

---
+++ front/dist/app.js
@@ -0,0 +1,71 @@
+"use strict";
+require("./prebundle/vendors-node_modules_taro_weapp_prebundle_taro-ui_js.js");
+require("./prebundle/vendors-node_modules_taro_weapp_prebundle_react-dom_js.js");
+require("./prebundle/vendors-node_modules_taro_weapp_prebundle_chunk-R3CD5KEQ_js.js");
+require("./prebundle/vendors-node_modules_taro_weapp_prebundle_chunk-XDFXK7K5_js.js");
+require("./prebundle/vendors-node_modules_taro_weapp_prebundle_chunk-IEBZOLZA_js.js");
+require("./prebundle/vendors-node_modules_taro_weapp_prebundle_tarojs_plugin-framework-react_dist_runtime_js.js");
+require("./prebundle/vendors-node_modules_taro_weapp_prebundle_tarojs_plugin-platform-weapp_dist_runtime_js.js");
+require("./prebundle/node_modules_taro_weapp_prebundle_tarojs_runtime_js.js");
+require("./prebundle/node_modules_taro_weapp_prebundle_tarojs_taro_js.js");
+require("./prebundle/remoteEntry.js");
+require("./prebundle/node_modules_taro_weapp_prebundle_react_jsx-runtime_js.js");
+require("./prebundle/node_modules_taro_weapp_prebundle_react_js.js");
+
+require("./runtime");
+
+(wx["webpackJsonp"] = wx["webpackJsonp"] || []).push([["app"],{
+
+/***/ "./src/app.ts":
+/*!********************!*\
+  !*** ./src/app.ts ***!
+  \********************/
+/***/ (function(__unused_webpack_module, __unused_webpack___webpack_exports__, __webpack_require__) {
+
+/* harmony import */ var _tarojs_plugin_platform_weapp_dist_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @tarojs/plugin-platform-weapp/dist/runtime */ "webpack/container/remote/@tarojs/plugin-platform-weapp/dist/runtime");
+/* harmony import */ var _tarojs_plugin_platform_weapp_dist_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_tarojs_plugin_platform_weapp_dist_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _tarojs_runtime__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @tarojs/runtime */ "webpack/container/remote/@tarojs/runtime");
+/* harmony import */ var _tarojs_runtime__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_tarojs_runtime__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _tarojs_plugin_framework_react_dist_runtime__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @tarojs/plugin-framework-react/dist/runtime */ "webpack/container/remote/@tarojs/plugin-framework-react/dist/runtime");
+/* harmony import */ var _tarojs_plugin_framework_react_dist_runtime__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_tarojs_plugin_framework_react_dist_runtime__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _tarojs_taro__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @tarojs/taro */ "webpack/container/remote/@tarojs/taro");
+/* harmony import */ var _tarojs_taro__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_tarojs_taro__WEBPACK_IMPORTED_MODULE_3__);
+Object(function webpackMissingModule() { var e = new Error("Cannot find module '!!../node_modules/@tarojs/taro-loader/lib/entry-cache.js?name=app!./app.ts'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! react */ "webpack/container/remote/react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_5__);
+/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! react-dom */ "webpack/container/remote/react-dom");
+/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_6___default = /*#__PURE__*/__webpack_require__.n(react_dom__WEBPACK_IMPORTED_MODULE_6__);
+
+
+
+
+
+
+
+
+
+
+
+var config = {"pages":["pages/home/index","pages/classify/index","pages/cart/index","pages/person/index","pages/login/index","pages/search/index"],"window":{"backgroundTextStyle":"light","navigationBarBackgroundColor":"#fff","navigationStyle":"custom","navigationBarTextStyle":"black"},"tabBar":{"custom":true,"list":[{"pagePath":"pages/home/index","text":"🏠"},{"pagePath":"pages/classify/index","text":"🗂️"},{"pagePath":"pages/cart/index","text":"🛒"},{"pagePath":"pages/person/index","text":"👤"}]}};
+_tarojs_runtime__WEBPACK_IMPORTED_MODULE_1__.window.__taroAppConfig = config
+var inst = App((0,_tarojs_plugin_framework_react_dist_runtime__WEBPACK_IMPORTED_MODULE_2__.createReactApp)(Object(function webpackMissingModule() { var e = new Error("Cannot find module '!!../node_modules/@tarojs/taro-loader/lib/entry-cache.js?name=app!./app.ts'"); e.code = 'MODULE_NOT_FOUND'; throw e; }()), react__WEBPACK_IMPORTED_MODULE_5__, (react_dom__WEBPACK_IMPORTED_MODULE_6___default()), config))
+
+;(0,_tarojs_taro__WEBPACK_IMPORTED_MODULE_3__.initPxTransform)({
+  designWidth: 750,
+  deviceRatio: {"375":2,"640":1.17,"750":1,"828":0.905},
+  baseFontSize: 20,
+  unitPrecision: undefined,
+  targetUnit: undefined
+})
+
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ __webpack_require__.O(0, ["common"], function() { return __webpack_exec__("./src/app.ts"); });
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ }
+]);;;
+//# sourceMappingURL=app.js.map
---
+++ front/dist/pages/person/index.js
@@ -0,0 +1,39 @@
+"use strict";
+(wx["webpackJsonp"] = wx["webpackJsonp"] || []).push([["pages/person/index"],{
+
+/***/ "./src/pages/person/index.tsx":
+/*!************************************!*\
+  !*** ./src/pages/person/index.tsx ***!
+  \************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack___webpack_exports__, __webpack_require__) {
+
+/* harmony import */ var _tarojs_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @tarojs/runtime */ "webpack/container/remote/@tarojs/runtime");
+/* harmony import */ var _tarojs_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_tarojs_runtime__WEBPACK_IMPORTED_MODULE_0__);
+Object(function webpackMissingModule() { var e = new Error("Cannot find module '!!../../../node_modules/@tarojs/taro-loader/lib/entry-cache.js?name=pages/person/index!./index.tsx'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+
+
+var config = {};
+
+
+
+var taroOption = (0,_tarojs_runtime__WEBPACK_IMPORTED_MODULE_0__.createPageConfig)(Object(function webpackMissingModule() { var e = new Error("Cannot find module '!!../../../node_modules/@tarojs/taro-loader/lib/entry-cache.js?name=pages/person/index!./index.tsx'"); e.code = 'MODULE_NOT_FOUND'; throw e; }()), 'pages/person/index', {root:{cn:[]}}, config || {})
+if (Object(function webpackMissingModule() { var e = new Error("Cannot find module '!!../../../node_modules/@tarojs/taro-loader/lib/entry-cache.js?name=pages/person/index!./index.tsx'"); e.code = 'MODULE_NOT_FOUND'; throw e; }()) && Object(function webpackMissingModule() { var e = new Error("Cannot find module '!!../../../node_modules/@tarojs/taro-loader/lib/entry-cache.js?name=pages/person/index!./index.tsx'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())) {
+  taroOption.behaviors = (taroOption.behaviors || []).concat(Object(function webpackMissingModule() { var e = new Error("Cannot find module '!!../../../node_modules/@tarojs/taro-loader/lib/entry-cache.js?name=pages/person/index!./index.tsx'"); e.code = 'MODULE_NOT_FOUND'; throw e; }()))
+}
+var inst = Page(taroOption)
+
+
+
+/* unused harmony default export */ var __WEBPACK_DEFAULT_EXPORT__ = (Object(function webpackMissingModule() { var e = new Error("Cannot find module '!!../../../node_modules/@tarojs/taro-loader/lib/entry-cache.js?name=pages/person/index!./index.tsx'"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ __webpack_require__.O(0, ["common"], function() { return __webpack_exec__("./src/pages/person/index.tsx"); });
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ }
+]);
+//# sourceMappingURL=index.js.map
